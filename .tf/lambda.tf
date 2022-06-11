13a6edae191b1c5db3ddeaa1023100234e749883
Shawn <shawnmcneill10@gmail.com>
2022-06-10 23:09:12 -0500

    third

---
+++ .tf/lambda.tf
@@ -0,0 +1,18 @@
+
+
+resource "aws_lambda_function" "spotify_analysis"{
+
+    filename = "../payload.zip"
+    function_name = "spotify_analysis"
+    handler = "avg_album_lenth_playlist.lambda_handler"
+    role = "${aws_iam_role.lambda_execution_role.arn}"
+    runtime="python3.8"
+    timeout = "300"
+
+    environment {
+    variables = {
+      SPOTIPY_CLIENT_ID = var.TF_VAR_SPOTIPY_CLIENT_ID,
+      SPOTIPY_CLIENT_SECRET = var.TF_VAR_SPOTIPY_CLIENT_SECRET
+    }
+  }
+}
